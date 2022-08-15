004fb377fd8225612c3e8238e346b14c3ca9f4a0
PersonalPhoenix <76667360+PersonalPhoenix@users.noreply.github.com>
2022-08-15 22:36:34 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
-from email.mime import message
+from cgitb import text
+from ctypes.wintypes import MSG
+from email import message, message_from_string
 import logging
-from typing import Dict
+import string
+from typing import Dict, Text
 from aiogram import Bot, Dispatcher, executor, types
 import requests
 from bs4 import BeautifulSoup
@@ -22,16 +25,16 @@ news_dict = {}
 dp = Dispatcher (bot)
 
 #возврат из под клавы
-catsMain = KeyboardButton ("Главное меню")
+catsMain = KeyboardButton ('Главное меню')
 
 #клава
-news = KeyboardButton("Получить свежачок")
+news_key = KeyboardButton('Получить свежачок')
 cats = KeyboardButton("Получить китика <3")
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(news,cats)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(news_key,cats)
 
 #под клава для китиков
-cats_animation = KeyboardButton("Флексящий китик")
-cats_picture = KeyboardButton("Статичный китик")
+cats_animation = KeyboardButton('Флексящий китик')
+cats_picture = KeyboardButton('Статичный китик')
 catsMenu = ReplyKeyboardMarkup (resize_keyboard=True).add(cats_animation,cats_picture,catsMain)
 
 #сообщение в CMD о старте
@@ -40,38 +43,41 @@ async def on_startup (_):
 
 #Стандартные комманды
 @dp.message_handler (commands = ["start"])
-async def start(message: types.Message):
+async def start  (message: types.Message):
     await bot.send_message( message.from_user.id, 'YEP начинаем',reply_markup = mainMenu)
     await bot.send_sticker(message.chat.id,"CAACAgIAAxkBAAEFiWli9TsP9B1BLa8HrEqL51EtH4UQEAACnhYAArpIyUtcjAeK6Rs_SykE")
 
-
 #парсер с выводом
-#if  message.text ("Получить свежачок"):
-@dp.message_handler (commands = ["news"])
+
+#@dp.message_handler (commands = ['Получить свежачок'])
 async def news (message: types.Message):
-        #await message.answer ("Пошло")
     url = ("https://www.cybersport.ru/tags/dota-2")
     r = requests.get (url)
     soup = BeautifulSoup (r.text, "lxml")
     rounded_block = soup.find_all (class_="container_qPDo5")
-
     for round in rounded_block:
         round_title = round.find ( class_="title_nSS03").text   
         round_data = round.find (class_="pub_AKjdn").text
         round_url = f'https://www.cybersport.ru/tags/dota-2{round.find_all("href")}'
-        #round_url = round.find (class_="link_CocWY")
-        #round_url = round.find_all ('link_CocWY'), round.get ('href') 
         print (round_data,round_title,round_url)
         news_dict = {
-        "time": round_data,
-        "title": round_title,
-        "url": round_url
-        }
+            "time": round_data,
+            "title": round_title,
+            "url": round_url
+            }
 
     with open ("news_dict.json","w",encoding='utf-8') as file:
         json.dump(news_dict, file, indent=4, ensure_ascii=False)
-   # await bot.send_message (message.chat.id,)
+
+#@dp.message_handler (commands = ['Получить китика <3'])
+async def kitik (message:types.Message):
+    await bot.send_sticker (message.from_user.id, "CAACAgIAAxkBAAEFiWli9TsP9B1BLa8HrEqL51EtH4UQEAACnhYAArpIyUtcjAeK6Rs_SykE")
+
+#обработка нажатий клавиатуры
+async def registor_client (dp: Dispatcher):
+    dp.register_message_handler (kitik, commands=['Получить китика <3'])
+    dp.register_message_handler (news, commands=['Получить свежачок'])
 
 #запуск бота    
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_polling (dp, skip_updates=True, on_startup=on_startup)
